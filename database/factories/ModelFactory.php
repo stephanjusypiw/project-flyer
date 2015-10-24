833a94c6ea9300a7d5bcbaa477bbd06eb2d4958d
Stephan Jusypiw <stephanj@internode.on.net>
2015-10-24 16:12:49 +1100

    06-Migrations-and-Models

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
@@ -17,5 +7,17 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Flyer::class, function (Faker\Generator $faker) {
+    return [
+        'street'        =>  $faker->streetAddress,
+        'city'          =>  $faker->city,
+        'zip'           =>  $faker->postcode,
+        'state'         =>  $faker->state,
+        'country'       =>  $faker->country,
+        'price'         =>  $faker->numberBetween(10000, 500000),
+        'description'   =>  $faker->paragraph(3),
     ];
 });
